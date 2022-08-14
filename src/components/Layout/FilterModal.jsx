7335a7c3ced965fe541882674eda6db3e49cf279
Farshid Karimi <farshidk49@gmail.com>
2022-08-14 18:01:48 +0430

    correct modal layout

--- src/components/Layout/FilterModal.jsx
+++ src/components/Layout/FilterModal.jsx
@@ -34,39 +34,41 @@ const FilterModal = () => {
 
           <div>
             <h5>Guests</h5>
-            <p>{noGuest ? "Add guests" : ctx.guests}</p>
+            <p className="disable">{noGuest ? "Add guests" : ctx.guests}</p>
           </div>
         </div>
 
-        <div className="modal-cities">
-          <ul>
-            <li onClick={() => setCityHandler("Helsinki")}>
-              <Icon name="location" /> <a>Helsinki, Finland</a>
-            </li>
-            <li onClick={() => setCityHandler("Turku")}>
-              <Icon name="location" /> <a>Turku, Finland</a>
-            </li>
-            <li onClick={() => setCityHandler("Oulu")}>
-              <Icon name="location" /> <a>Oulu, Finland</a>
-            </li>
-            <li onClick={() => setCityHandler("Vaasa")}>
-              <Icon name="location" /> <a>Vaasa, Finland</a>
-            </li>
-          </ul>
-        </div>
+        <div className="modal-add-part">
+          <div className="modal-cities">
+            <ul>
+              <li onClick={() => setCityHandler("Helsinki")}>
+                <Icon name="location" /> <a>Helsinki, Finland</a>
+              </li>
+              <li onClick={() => setCityHandler("Turku")}>
+                <Icon name="location" /> <a>Turku, Finland</a>
+              </li>
+              <li onClick={() => setCityHandler("Oulu")}>
+                <Icon name="location" /> <a>Oulu, Finland</a>
+              </li>
+              <li onClick={() => setCityHandler("Vaasa")}>
+                <Icon name="location" /> <a>Vaasa, Finland</a>
+              </li>
+            </ul>
+          </div>
 
-        <div className="modal-add-guests">
-          <h5>Adults</h5>
+          <div className="modal-add-guests">
+            <h5>Adults</h5>
 
-          <p className="disable">Ages 13 or above</p>
+            <p className="disable">Ages 13 or above</p>
 
-          <p>{ctx.guests}</p>
+            <p>{ctx.guests}</p>
 
-          <h5>Children</h5>
+            <h5>Children</h5>
 
-          <p className="disable">Ages 2-12</p>
+            <p className="disable">Ages 2-12</p>
 
-          <p>{ctx.guests}</p>
+            <p>{ctx.guests}</p>
+          </div>
         </div>
 
         <div className="modal-search-btn">
